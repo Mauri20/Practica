05f7cc201bd623c58dabe175a60844e75b422195
Mauri20 <69651999+Mauri20@users.noreply.github.com>
2020-09-08 13:40:42 -0600

    Hola Mundo
    Primera adicion de codigo a Github desde Netbeans

--- Practica/src/practica/Practica.java
+++ Practica/src/practica/Practica.java
@@ -16,6 +16,8 @@ public class Practica {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String hola="Hola mundo";
+        System.out.println(hola);
     }
     
 }
